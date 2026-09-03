549e78206fc07d5d6a4ad1235c315dff0b097b82
CK627 <3340324320@qq.com>
2026-09-03 23:57:53 +0800

    feat(ground-robot): add reverse route return script

---
+++ GroundRobot/Code/tasks/ceshi/FixedGripTest/return_origin.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+# coding=utf8
+"""按 record_route.py 录制的 fixed_route.json 反向回到原点。"""
+
+import argparse
+import json
+import os
+import sys
+import time
+
+_PKG_ROOT = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.dirname(os.path.abspath(__file__)))))
+if _PKG_ROOT not in sys.path:
+    sys.path.insert(0, _PKG_ROOT)
+
+from agcs_lib import make_board, make_ik
+
+
+def main():
+    parser = argparse.ArgumentParser(description='反向执行录制路线回到原点')
+    parser.add_argument('--route', default=os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'fixed_route.json'),
+        help='record_route.py 生成的路线文件')
+    args = parser.parse_args()
+
+    with open(args.route, 'r', encoding='utf-8') as f:
+        actions = json.load(f)
+
+    board = make_board()
+    ik = make_ik(board)
+
+    print('共 %d 个动作，开始反向回原点' % len(actions), flush=True)
+    ik.stand(ik.initial_pos, t=500)
+    time.sleep(0.5)
+
+    for i, act in enumerate(reversed(actions), 1):
+        name = act.get('action')
+        speed = int(act.get('speed', 50))
+
+        if name == 'forward':
+            step = int(act.get('step', 50))
+            print('%d/%d back %d' % (i, len(actions), step), flush=True)
+            ik.back(ik.initial_pos, 2, step, speed, 1)
+        elif name == 'back':
+            step = int(act.get('step', 50))
+            print('%d/%d forward %d' % (i, len(actions), step), flush=True)
+            ik.go_forward(ik.initial_pos, 2, step, speed, 1)
+        elif name == 'turn_left':
+            angle = int(act.get('angle', 30))
+            print('%d/%d turn_right %d' % (i, len(actions), angle), flush=True)
+            ik.turn_right(ik.initial_pos, 2, angle, speed, 1)
+        elif name == 'turn_right':
+            angle = int(act.get('angle', 30))
+            print('%d/%d turn_left %d' % (i, len(actions), angle), flush=True)
+            ik.turn_left(ik.initial_pos, 2, angle, speed, 1)
+        elif name == 'stand':
+            print('%d/%d stand' % (i, len(actions)), flush=True)
+            ik.stand(ik.initial_pos, t=500)
+        else:
+            print('跳过未知动作: %s' % act, flush=True)
+
+        time.sleep(0.08)
+
+    ik.stand(ik.initial_pos, t=500)
+    print('反向路线执行完成', flush=True)
+
+
+if __name__ == '__main__':
+    main()
